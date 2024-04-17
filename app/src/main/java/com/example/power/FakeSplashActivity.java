1b5375f366ecf7d06110e7a9c08262172c7c51d6
Mikey <astanovykh@gmail.com>
2024-04-17 19:43:47 +0500

    upgrade

--- app/src/main/java/com/example/power/FakeSplashActivity.java
+++ app/src/main/java/com/example/power/FakeSplashActivity.java
@@ -1,24 +1,39 @@
 package com.example.power;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.LinearGradient;
+import android.graphics.Shader;
 import android.os.Bundle;
 import android.os.Handler;
+import android.text.TextPaint;
+import android.widget.TextView;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
 import androidx.core.splashscreen.SplashScreen;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class FakeSplashActivity extends AppCompatActivity {
 
+    TextView loadtext;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_splash);
+        loadtext = findViewById(R.id.textview);
+        loadtext.setText(getResources().getString(R.string.logo_text));
 
+        TextPaint paint = loadtext.getPaint();
+        float height = paint.measureText(getResources().getString(R.string.logo_text));
+
+        Shader textShader = new LinearGradient(0,0,loadtext.getTextSize(), height ,
+                new int[]{
+                        Color.parseColor("#000000"),
+                        Color.parseColor("#8AC1D9"),
+                        Color.parseColor("#8AC1D9"),
+                        Color.parseColor("#8AC1D9"),
+            },null, Shader.TileMode.CLAMP);
+        loadtext.getPaint().setShader(textShader);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
